86b2118a7b949c66433d7c2c2530a08bd95f0e54
songkungsuk <songkungsug@gmail.com>
2023-07-26 18:18:27 +0900

    sadsadsa

--- src/main/java/com/game/controller/UserInfoServlet.java
+++ src/main/java/com/game/controller/UserInfoServlet.java
@@ -16,101 +16,95 @@ import com.game.common.CommonView;
 import com.game.service.UserInfoService;
 import com.game.service.impl.UserInfoServiceImpl;
 
-
 @WebServlet("/user-info/*")
 public class UserInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	UserInfoService urService = new UserInfoServiceImpl();
-	
-    public UserInfoServlet() {
-    	System.out.println("userInfoServlet create");
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public UserInfoServlet() {
+		System.out.println("userInfoServlet create");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String cmd = CommonView.getcmd(request);
-		
-		if("list".equals(cmd)) {
-			request.setAttribute("userList", urService.selectUserInfoList(null)); 
+
+		if ("list".equals(cmd)) {
+			request.setAttribute("userList", urService.selectUserInfoList(null));
 		}
-		if("view".equals(cmd)) {
+		if ("view".equals(cmd)) {
 			String uiNum = request.getParameter("uiNum");
 			request.setAttribute("user", urService.selectUserInfo(uiNum));
 		}
-		if("update".equals(cmd)) {
+		if ("update".equals(cmd)) {
 			String uiNum = request.getParameter("uiNum");
 			request.setAttribute("user", urService.selectUserInfo(uiNum));
 		}
-		
+
 		CommonView.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.setCharacterEncoding("UTF-8");
-		Map<String, String> user2 = new HashMap<String, String>();
-		user2.put("UI_NAME", request.getParameter("UI_NAME"));
-		user2.put("UI_ID", request.getParameter("UI_ID"));
-		user2.put("UI_PWD", request.getParameter("UI_PWD"));
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		
+		request.setCharacterEncoding("UTF-8");
 		String cmd = CommonView.getcmd(request);
-		if("insert".equals(cmd)) {
-			Map<String, String> user = new HashMap<String, String>();
-			user.put("UI_NAME", request.getParameter("UI_NAME"));
-			user.put("UI_ID", request.getParameter("UI_ID"));
-			user.put("UI_PWD", request.getParameter("UI_PWD"));
-			user.put("UI_IMG_PATH", request.getParameter("UI_IMG_PATH"));
-			user.put("UI_DESC", request.getParameter("UI_DESC"));
+		Map<String, String> user = new HashMap<String, String>();
+		user.put("UI_NAME", request.getParameter("UI_NAME"));
+		user.put("UI_ID", request.getParameter("UI_ID"));
+		user.put("UI_PWD", request.getParameter("UI_PWD"));
+		if (request.getParameter("UI_BIRTH") != null) {
 			user.put("UI_BIRTH", request.getParameter("UI_BIRTH").replace("-", ""));
-			
+		}
+		user.put("UI_IMG_PATH", request.getParameter("UI_IMG_PATH"));
+		user.put("UI_DESC", request.getParameter("UI_DESC"));
+		user.put("UI_NUM", request.getParameter("UI_NUM"));
+		if ("insert".equals(cmd)) {
+
 			int result = urService.insertUserInfo(user);
 			request.setAttribute("msg", "유저 등록 성공");
 			request.setAttribute("url", "/user-info/list");
-			
-			if(result != 1) {
+
+			if (result != 1) {
 				request.setAttribute("msg", "유저 등록 실패");
 				request.setAttribute("url", "/user-info/insert");
 			}
 		}
-		if("update".equals(cmd)) {
+		if ("update".equals(cmd)) {
 
-			int result = urService.updateUserInfo(user2);
-			user2.put("UI_BIRTH", request.getParameter("UI_BIRTH").replace("-", ""));
-			user2.put("UI_IMG_PATH", request.getParameter("UI_IMG_PATH"));
-			user2.put("UI_DESC", request.getParameter("UI_DESC"));
-			user2.put("UI_NUM", request.getParameter("UI_NUM"));
+			int result = urService.updateUserInfo(user);
+
 			request.setAttribute("msg", "유저 수정 성공");
 			request.setAttribute("url", "/user-info/list");
-			
-			if(result != 1) {
+
+			if (result != 1) {
 				request.setAttribute("msg", "유저 수정 실패");
 				request.setAttribute("url", "/user-info/list");
 			}
 		}
-		if("delete".equals(cmd)) {
+		if ("delete".equals(cmd)) {
 			String uiNum = request.getParameter("UI_NUM");
-			
+
 			int result = urService.deleteUserInfo(uiNum);
 			request.setAttribute("msg", "유저 삭제 성공");
 			request.setAttribute("url", "/user-info/list");
-			
-			if(result != 1) {
+
+			if (result != 1) {
 				request.setAttribute("msg", "유저 삭제 실패");
 				request.setAttribute("url", "/user-info/list");
 			}
 		}
-		if("login".equals(cmd)) {
+		if ("login".equals(cmd)) {
 			request.setAttribute("msg", "아이디나 비밀번호를 확인해주세요");
 			request.setAttribute("url", "/user-info/login");
 			HttpSession session = request.getSession();
-			boolean login = urService.login(user2, session);
-			if(login) {
+			boolean login = urService.login(user, session);
+			if (login) {
 				request.setAttribute("msg", "로그인성공");
 				request.setAttribute("url", "/");
 			}
 		}
-		
-		
+
 		CommonView.forwardMessage(request, response);
 	}
 
